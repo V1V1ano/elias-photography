234098ed30f0879b27c6231890cbd3eaeb19741d
V1V1ano <vivien.serve@fh-potsdam.de>
2026-02-13 16:08:26 +0100

    preliminary footer and header

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -12,40 +12,20 @@
   </main>
 
   <footer class="footer">
-    <div class="grid">
-      <div class="column" style="--columns: 8">
-        <h2><a href="https://getkirby.com">Made with Kirby</a></h2>
-        <p>
-          Kirby: the file-based CMS that adapts to any project, loved by developers and editors alike
-        </p>
+      <div class="logo">
+        <span class="logo-inline"> call: +49 17672270664 </span>
+        <a class="logo-inline" href="mailto:elias.bernhardt@outlook.com">mail: elias.bernhardt@outlook.com</a>
       </div>
-      <div class="column" style="--columns: 2">
-        <h2>Pages</h2>
-        <ul>
-          <?php foreach ($site->children()->listed() as $example): ?>
-          <li><a href="<?= $example->url() ?>"><?= $example->title()->esc() ?></a></li>
-          <?php endforeach ?>
-        </ul>
+
+
+      <!-- need to add pages imprint and data privacy -->
+      <div class="logo">
+        <span class="logo-inline"> imprint </span>
+        <span class="logo-inline"> data privacy</span>
       </div>
-      <div class="column" style="--columns: 2">
-        <h2>Kirby</h2>
-        <ul>
-          <li><a href="https://getkirby.com">Website</a></li>
-          <li><a href="https://getkirby.com/docs">Docs</a></li>
-          <li><a href="https://forum.getkirby.com">Forum</a></li>
-          <li><a href="https://chat.getkirby.com">Chat</a></li>
-          <li><a href="https://github.com/getkirby">GitHub</a></li>
-        </ul>
-      </div>
-    </div>
+      
   </footer>
 
-  <?= js([
-    'assets/js/prism.js',
-    'assets/js/lightbox.js',
-    'assets/js/index.js',
-    '@auto'
-  ]) ?>
 
 </body>
 </html>
