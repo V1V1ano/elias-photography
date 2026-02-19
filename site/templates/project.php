2dcfcf6c966a0c20021fada5fd73582bbd2a6bdc
V1V1ano <vivien.serve@fh-potsdam.de>
2026-02-19 12:58:34 +0100

    first setup project page, only single images possible

---
+++ site/templates/project.php
@@ -0,0 +1,42 @@
+<?php snippet('header') ?>
+
+<article class="project">
+  <h1><?= $page->title()->esc() ?></h1>
+
+  <div class="project-metadata"> 
+    <span class="project-metadata-row1"> 
+      <p> <?= $page->type()->esc() ?> </p>
+      <p> <?= $page->occasion()->esc() ?> </p>
+    </span> 
+    <span class="project-metadata-row1"> 
+      <p> <?= $page->year()->esc() ?> </p>
+      <p> <?= $page->city()->esc() ?> </p>
+      <p> <?= $page->place()->esc() ?> </p>
+    </span> 
+  </div>
+
+  <?php
+    // all images in this project folder
+    $images = $page->images()->sortBy('sort', 'asc');
+  ?>
+
+  <?php if ($images->isNotEmpty()): ?>
+    <section class="project-images">
+      <?php foreach ($images as $image): ?>
+        <figure class="project-image">
+          <img
+            src="<?= $image->url() ?>"
+            alt="<?= $image->alt()->or($image->filename())->esc() ?>"
+          >
+        </figure>
+      <?php endforeach ?>
+    </section>
+  <?php endif ?>
+
+  <?php if ($page->description()->isNotEmpty()): ?>
+      <p class="project-desc"><?= $page->description()->kt() ?></p>
+  <?php endif ?>
+
+</article>
+
+<?php snippet('footer') ?>
