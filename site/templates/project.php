e6faa874cdb9abd279204afa396d9059d52196a1
V1V1ano <vivien.serve@fh-potsdam.de>
2026-02-20 14:49:35 +0100

    add 1 and 2 picture block functionality to projects

--- site/templates/project.php
+++ site/templates/project.php
@@ -3,39 +3,70 @@
 <article class="project">
   <h1 class="project-title"><?= $page->title()->esc() ?></h1>
 
-  <div class="project-metadata"> 
-    <span class="project-metadata-row1"> 
-      <p> <?= $page->type()->esc() ?> </p>
-      <p> <?= $page->occasion()->esc() ?> </p>
-    </span> 
-    <span class="project-metadata-row1"> 
-      <p> <?= $page->year()->esc() ?> </p>
-      <p> <?= $page->city()->esc() ?> </p>
-      <p> <?= $page->place()->esc() ?> </p>
-    </span> 
+  <div class="project-metadata">
+    <span class="project-metadata-row1">
+      <p><?= $page->type()->esc() ?></p>
+      <p><?= $page->occasion()->esc() ?></p>
+    </span>
+    <span class="project-metadata-row1">
+      <p><?= $page->year()->esc() ?></p>
+      <p><?= $page->city()->esc() ?></p>
+      <p><?= $page->place()->esc() ?></p>
+    </span>
   </div>
 
-  <?php
-    // all images in this project folder
-    $images = $page->images()->sortBy('sort', 'asc');
-  ?>
-
-  <?php if ($images->isNotEmpty()): ?>
+  <?php if ($page->photoSections()->isNotEmpty()): ?>
     <section class="project-images">
-      <?php foreach ($images as $image): ?>
-        <figure class="project-image">
-          <img
-            src="<?= $image->url() ?>"
-            alt="<?= $image->alt()->or($image->filename())->esc() ?>"
-          >
-        </figure>
-      <?php endforeach ?>
+      <?php foreach ($page->photoSections()->toBlocks() as $block): ?>
+
+        <?php if ($block->type() === 'oneImage'): ?>
+          <?php $img = $block->image()->toFile(); ?>
+          <?php if ($img): ?>
+            <figure class="project-image project-image--one">
+              <img
+                src="<?= $img->url() ?>"
+                alt="<?= $img->alt()->or($img->filename())->esc() ?>"
+                loading="lazy"
+              >
+            </figure>
+          <?php endif; ?>
+
+        <?php elseif ($block->type() === 'twoImages'): ?>
+          <?php
+            $left  = $block->left()->toFile();
+            $right = $block->right()->toFile();
+          ?>
+          <?php if ($left || $right): ?>
+            <figure class="project-image project-image--two">
+              <?php if ($left): ?>
+                <img
+                  class="project-image__img"
+                  src="<?= $left->url() ?>"
+                  alt="<?= $left->alt()->or($left->filename())->esc() ?>"
+                  loading="lazy"
+                >
+              <?php endif; ?>
+
+              <?php if ($right): ?>
+                <img
+                  class="project-image__img"
+                  src="<?= $right->url() ?>"
+                  alt="<?= $right->alt()->or($right->filename())->esc() ?>"
+                  loading="lazy"
+                >
+              <?php endif; ?>
+            </figure>
+          <?php endif; ?>
+
+        <?php endif; ?>
+
+      <?php endforeach; ?>
     </section>
-  <?php endif ?>
+  <?php endif; ?>
 
   <?php if ($page->description()->isNotEmpty()): ?>
-      <p class="project-desc"><?= $page->description()->kt() ?></p>
-  <?php endif ?>
+    <p class="project-desc"><?= $page->description()->kt() ?></p>
+  <?php endif; ?>
 
 </article>
 
