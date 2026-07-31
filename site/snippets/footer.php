55bf1306c52a5401b4c6b6dde57a94ea7a34ea32
V1V1ano <vivien.serve@fh-potsdam.de>
2026-07-31 20:07:15 +0200

    test footer.php

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -14,7 +14,7 @@
   <footer class="footer">
       <div class="logo">
         <span class="logo-inline">call:&nbsp;+49&nbsp;17672270664</span>
-        <a class="logo-inline" href="mailto:hallo@eliasbernhardt.de">mail: hund@eliasbernhardt.de</a>
+        <a class="logo-inline" href="mailto:hallo@eliasbernhardt.de">mail: hallo@eliasbernhardt.de</a>
       </div>
 
       <?php if ($page->template()->name() === 'home'): ?>
